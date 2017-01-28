f48f6f9a77351e0c9a8a05b20cc1d67abdcd362c
Kirill Skovpen <Kirill.Skovpen@cern.ch>
2017-01-28 11:58:40 +0100

    update submission scripts

--- FlatTreeProducer/test/PROD/submit.zsh
+++ FlatTreeProducer/test/PROD/submit.zsh
@@ -2,7 +2,7 @@
 
 slist="list.txt"
 pset="crabConfigTemplate.py"
-ver="Sailfish-patch1-v20160913"
+ver="Sailfish-patch3-v20170128"
 prodv="/store/user/kskovpen/FlatTree/${ver}/"
 
 rm -f crabConfig.py*
@@ -20,7 +20,7 @@ do
   spl=($(echo $i | tr "/" "\n"))
   pubdn=$(echo "${spl[2]}_${spl[3]}" | sed 's%-%_%g')
   nam=$(echo "${spl[1]}" | sed 's%-%_%g')
-  reqn=$(echo "${nam}_${pubdn}" | sed 's%_RunIISpring16MiniAODv2.*%%g')
+  reqn=$(echo "${nam}_${pubdn}" | sed 's%_RunIISummer16MiniAODv2.*%%g')
   cat ${pset} | sed "s%INPUTDATASET%${i}%g" \
   | sed "s%OUTLFN%${prodv}%g" \
   | sed "s%REQUESTNAME%${reqn}%g" \
